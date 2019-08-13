1951db1b6fc590402405e5d387f5c73cba8d4591
Amaury Diaz <amauryuh@gmail.com>
2019-08-13 14:19:00 -0400

    Updating static website

---
+++ precache-manifest.28428d3b669fb9f7b8415e27d82ec8ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be11cbe52d2880faf9326ad641cc15a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f979a03d51aceb98c152",
+    "url": "/static/css/main.f4074c60.chunk.css"
+  },
+  {
+    "revision": "89e6e88f318a99b02747",
+    "url": "/static/js/2.db0e9e43.chunk.js"
+  },
+  {
+    "revision": "f979a03d51aceb98c152",
+    "url": "/static/js/main.46f1ad1e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
